fa44fb9ef5e23a69da191fa8e7e1fb6b3dbf214d
gymnast25 <231230045@unmuhpnk.ac.id>
2025-05-19 19:37:39 +0700

    Tampilan Blade Template

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    
+    public function index()
+    {
+        //
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show(Book $book)
+    {
+        //
+    }
+
+    
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, Book $book)
+    {
+        //
+    }
+
+    
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
